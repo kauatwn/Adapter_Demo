04d12c7bf4cf8c8f768c3a2a998b364e5d889a93
kauatwn <kaualopescontato@gmail.com>
2024-10-18 16:12:11 -0300

    Criando entidade OrderItem

---
+++ Domain/Entities/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class OrderItem(int id, string productName, int quantity, double price)
+{
+    public int Id { get; set; } = id;
+    public string ProductName { get; set; } = productName;
+    public int Quantity { get; set; } = quantity;
+    public double Price { get; set; } = price;
+}
