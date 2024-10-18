eec68902ea6cf02466b7ce7937261852f76bd03a
kauatwn <kaualopescontato@gmail.com>
2024-10-18 16:25:21 -0300

    Criando entidade Order

---
+++ Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public class Order(int id, string customerName, List<OrderItem> items)
+{
+    public int Id { get; set; } = id;
+    public string CustomerName { get; set; } = customerName;
+    public List<OrderItem> Items { get; set; } = items;
+
+    public double Total => Items.Sum(item => item.Price * item.Quantity);
+}
