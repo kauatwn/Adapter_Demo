a41e15ee61cafdfc82c82e4755a0312549020df6
kauatwn <kaualopescontato@gmail.com>
2024-10-19 11:06:16 -0300

    Criando PaymentAdapter

---
+++ src/Infrastructure/Adapters/PaymentAdapter.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces.Gateways;
+using Domain.Interfaces.Services;
+
+namespace Infrastructure.Adapters;
+
+public class PaymentAdapter(IPaymentGateway paymentGateway) : IPaymentService
+{
+    private IPaymentGateway PaymentGateway { get; } = paymentGateway;
+
+    public void ProcessPayment(double amount)
+    {
+        Console.WriteLine("Processando pagamento...");
+
+        if (amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater than zero");
+        }
+
+        PaymentGateway.MakePayment(amount);
+    }
+}
