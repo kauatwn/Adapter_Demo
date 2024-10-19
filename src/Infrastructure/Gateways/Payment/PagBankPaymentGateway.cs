5b1396283663d6bbc9e073cdb02a46b407e331d9
kauatwn <kaualopescontato@gmail.com>
2024-10-19 11:08:19 -0300

    Criando PagBankPaymentGateway

---
+++ src/Infrastructure/Gateways/Payment/PagBankPaymentGateway.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces.Gateways;
+
+namespace Infrastructure.Gateways.Payment;
+
+public class PagBankPaymentGateway : IPaymentGateway
+{
+    public void MakePayment(double amount)
+    {
+        Console.WriteLine($"Pagamento de {amount:C} realizado via PagBank");
+    }
+}
