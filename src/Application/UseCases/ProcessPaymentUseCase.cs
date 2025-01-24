fd73b01f89c9e5d0cde66293f31c37457d5fdd56
kauatwn <kaualopescontato@gmail.com>
2025-01-24 16:40:22 -0300

    Criar classe ProcessPaymentUseCase

---
+++ src/Application/UseCases/ProcessPaymentUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Domain.Interfaces.Services;
+
+namespace Application.UseCases;
+
+public class ProcessPaymentUseCase(IPaymentService service) : IProcessPaymentUseCase
+{
+    private IPaymentService Service { get; } = service;
+
+    public Order Execute(Order order)
+    {
+        Service.ProcessPayment(order.Total);
+
+        return order;
+    }
+}
