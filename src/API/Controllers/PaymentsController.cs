20b6b7b94f97f30313ce2091f0027a73b9649e5d
kauatwn <kaualopescontato@gmail.com>
2025-01-27 11:41:02 -0300

    Criar PaymentsController

---
+++ src/API/Controllers/PaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions.UseCases;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PaymentsController : ControllerBase
+{
+    [HttpPost("ProcessPayment")]
+    [ProducesResponseType<Order>(StatusCodes.Status200OK)]
+    public IActionResult ProcessPayment(IProcessPaymentUseCase useCase, Order order)
+    {
+        return Ok(useCase.Execute(order));
+    }
+}
